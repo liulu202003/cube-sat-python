64f955e7e1cd63f64f1d36ce47c6ba79b8ac2d2a
Alan Johnston <alan.b.johnston@gmail.com>
2020-04-09 16:30:37 +0100

    check for command line params

--- main.py
+++ main.py
@@ -1,6 +1,29 @@
 import sys
 
+FSK = 0
+BPSK = 1
+AFSK = 2
+
+mode = FSK
+cycle = False
+
 if __name__ == "__main__":
-    print(f"Arguments count: {len(sys.argv)}")
-    for i, arg in enumerate(sys.argv):
-        print(f"Argument {i:>6}: {arg}")
+#    print(f"Arguments count: {len(sys.argv)}")
+#    for i, arg in enumerate(sys.argv):
+#        print(f"Argument {i:>6}: {arg}")
+#
+    if (len(sys.argv)) > 0:
+#        print("There are arguments!")
+        if (sys.argv[1][0] == 'a'):
+            mode = AFSK
+            print("AFSK") 
+        if (sys.argv[1][0] == 'f'):
+            mode = FSK
+            print("FSK") 
+        if (sys.argv[1][0] == 'b'):
+            mode = BPSK
+            print("BPSK")
+        if (sys.argv[1][0] == 'c'):
+            cycle = True 
+            print("Cycle mode on!")
+
